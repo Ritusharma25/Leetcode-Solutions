445750460bebc8a9b675d9571b585f1c10f16896
Ritu Sharma <52835770+Ritusharma25@users.noreply.github.com>
2025-06-21 22:18:09 +0530

    Update Delete_Duplicate_Emails.sql

--- SQL/Delete_Duplicate_Emails.sql
+++ SQL/Delete_Duplicate_Emails.sql
@@ -9,7 +9,7 @@
 +-------------+---------+
 id is the primary key (column with unique values) for this table.
 Each row of this table contains an email. The emails will not contain uppercase letters.
- 
+  
 
 Write a solution to delete all duplicate emails, keeping only one unique email with the smallest id.
 
